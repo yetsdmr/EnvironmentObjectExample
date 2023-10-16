69995cf016621aeb413c1538ac21ed7df285fa9e
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-16 18:38:00 +0300

    Initial Commit

---
+++ EnvironmentObjectExample/EnvironmentObjectExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectExampleApp.swift
+//  EnvironmentObjectExample
+//
+//  Created by Yunus Emre Taşdemir on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
